9fff915e09f78e2ff3da5af90f594b5707e46113
ConorNield <conornield@gmail.com>
2024-09-10 13:23:15 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Use HashRouter here
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
+import ReactGA from 'react-ga';
 import NavBar from './components/NavBar/NavBar';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills/Skills';
 import Blogs from './pages/Blogs';
 import Footer from './components/Footer/Footer';
-import BattingOrderBlog from "./assets/configs/battingOrderBlog.js";
+import BattingOrderBlog from "./assets/configs/battingOrderBlog.js"
 import Stat429FinalProject from './assets/configs/cornYieldPrediction.js';
 import SoybeanGARCH from './assets/configs/soybeanGARCH.js';
 import Lottery from './assets/configs/NBALottery.js';
-import Picks from './assets/configs/NBA_Picks.csv';
+import Picks from './assets/configs/NBA_Picks.csv'
 import './App.css';
+
+import React from 'react';
+
+if (typeof process.env.REACT_APP_TRACKING_ID !== 'undefined') {
+    ReactGA.initialize(process.env.REACT_APP_TRACKING_ID);
+}
 
 function App() {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-            <Router basename="/portfolio"> {/* HashRouter with basename */}
+            <Router>
                 <NavBar />
                 <Container sx={{ flex: 1, py: 3 }}>
                     <Routes>
-                        <Route path="/" exact element={<Home />} />
+                        <Route path="/portfolio" exact element={<Home />} />
                         <Route path="/projects" exact element={<Projects />} />
                         <Route path="/blogs" exact element={<Blogs />} />
                         <Route path="/skills" exact element={<Skills />} />
@@ -39,3 +45,5 @@ function App() {
 }
 
 export default App;
+
+
